071a54f8505626cea6b0dc2747c55ab0ce2ffae1
Arjun A <ajitharjun@gmail.com>
2012-10-09 12:31:09 +0530

    Started developing in Java using netbeans

---
+++ JavaNB/qResp/src/qresp/QResp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qresp;
+
+/**
+ *
+ * @author Arjun
+ */
+public class QResp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
